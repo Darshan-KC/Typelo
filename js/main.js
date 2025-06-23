968a19c6ebfe290ca69231d74e116f26893ad38b
Darshan KC <darshanbahadurchhetri@gmail.com>
2025-06-23 21:41:17 +0545

    Feat: working in main js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
 
 import { getRandomQuote } from './utils.js';
 import { renderQuote, resetUI } from './ui.js';
+
+
+const quoteElement = document.getElementById('quote');
+const inputElement = document.getElementById('input-area');
+const startBtn = document.getElementById('start-btn');
+const restartBtn = document.getElementById('restart-btn');
+
+let currentQuote = "";
